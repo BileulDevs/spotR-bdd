65925d43d27ba6e082e4345be2c1d3112ca2dce3
Bilel <91191386+BileulDevs@users.noreply.github.com>
2025-05-02 13:22:30 +0200

    feat/metrics (#1)

--- config/logger.js
+++ config/logger.js
@@ -2,7 +2,10 @@ const winston = require("winston");
 
 const logger = winston.createLogger({
     level: 'info',
-    format: winston.format.json(),
+    format: winston.format.combine(
+        winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+        winston.format.json()
+    ),
     defaultMeta: { service: 'bdd-service' },
     transports: [
         new winston.transports.File({ filename: './storage/warnings.log', level: 'warn' }),
